0acfbaf1af7f6464636fbbb037272d2461d39eb1
kgv <kgv@users.noreply.github.com>
2025-01-13 16:24:51 +0300

    Bump version to 0.1.0

---
+++ src/error.rs
@@ -0,0 +1,16 @@
+use polars::prelude::PolarsError;
+use thiserror::Error;
+
+/// Result
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+/// Error
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(r#"metadata date "{0}""#)]
+    Date(#[from] chrono::ParseError),
+    #[error(r#"metadata version "{0}""#)]
+    Version(#[from] semver::Error),
+    #[error(r#"polars metadata "{0}""#)]
+    Polars(#[from] PolarsError),
+}
