6ee66b509abefbdcb93ce91addc6b3c729a55d13
kgv <kgv@users.noreply.github.com>
2025-05-24 12:51:08 +0300

    Bump version to 0.6.0

--- src/egui.rs
+++ src/egui.rs
@@ -1,12 +1,14 @@
 use crate::{AUTHORS, DATE, DESCRIPTION, Metadata, NAME, VERSION};
-use chrono::Local;
-use egui::{DragValue, Grid, Label, Response, TextEdit, Ui};
+use chrono::NaiveDate;
+use egui::{
+    DragValue, Grid, Label, Response, TextEdit, Ui,
+    cache::{ComputerMut, FrameCache},
+};
 use egui_extras::{Column, DatePickerButton, TableBuilder};
 use egui_phosphor::regular::{MINUS, PLUS};
-use std::borrow::{Borrow, BorrowMut};
-
-use chrono::NaiveDate;
 use semver::Version;
+use std::borrow::{Borrow, BorrowMut};
+use tracing::error;
 
 pub const DATE_FORMAT: &str = "%Y-%m-%d";
 
@@ -86,27 +88,37 @@ impl<T: Borrow<Metadata>> MetadataWidget<T> {
                 let metadata = self.metadata.borrow();
                 if self.name {
                     ui.label("Name");
-                    ui.label(&metadata[NAME]);
+                    if let Some(name) = metadata.get(NAME) {
+                        ui.label(name);
+                    }
                     ui.end_row();
                 }
                 if self.description {
                     ui.label("Description");
-                    ui.add(Label::new(&metadata[DESCRIPTION]).truncate());
+                    if let Some(description) = metadata.get(DESCRIPTION) {
+                        ui.add(Label::new(description).truncate());
+                    }
                     ui.end_row();
                 }
                 if self.authors {
                     ui.label("Authors");
-                    ui.label(&metadata[AUTHORS]);
+                    if let Some(authors) = metadata.get(AUTHORS) {
+                        ui.label(authors);
+                    }
                     ui.end_row();
                 }
                 if self.version {
                     ui.label("Version");
-                    ui.label(&metadata[VERSION]);
+                    if let Some(version) = metadata.get(VERSION) {
+                        ui.label(version);
+                    }
                     ui.end_row();
                 }
                 if self.date {
                     ui.label("Date");
-                    ui.label(&metadata[DATE]);
+                    if let Some(date) = metadata.get(DATE) {
+                        ui.label(date);
+                    }
                     ui.end_row();
                 }
             })
@@ -120,8 +132,7 @@ impl<T: BorrowMut<Metadata>> MetadataWidget<T> {
         ui.style_mut().visuals.collapsing_header_frame = true;
         let height = ui.spacing().interact_size.y;
         TableBuilder::new(ui)
-            .resizable(true)
-            .column(Column::auto())
+            .resizable(false)
             .column(Column::auto())
             .column(Column::remainder())
             .body(|mut body| {
@@ -133,23 +144,12 @@ impl<T: BorrowMut<Metadata>> MetadataWidget<T> {
                             ui.label("Name");
                         });
                         row.col(|ui| {
-                            let mut checked = metadata.contains_key(NAME);
-                            if ui.checkbox(&mut checked, "").changed() {
-                                if checked {
-                                    metadata.insert(NAME.to_owned(), String::new());
-                                } else {
-                                    metadata.remove(NAME);
-                                }
-                            }
-                        });
-                        row.col(|ui| {
-                            if let Some(name) = metadata.get_mut(NAME) {
-                                if ui
-                                    .add(TextEdit::singleline(name).desired_width(f32::INFINITY))
-                                    .lost_focus()
-                                {
-                                    *name = name.trim().to_owned();
-                                }
+                            let name = metadata.entry(NAME.to_owned()).or_default();
+                            if ui
+                                .add(TextEdit::singleline(name).desired_width(f32::INFINITY))
+                                .lost_focus()
+                            {
+                                *name = name.trim().to_owned();
                             }
                         });
                     });
@@ -161,26 +161,12 @@ impl<T: BorrowMut<Metadata>> MetadataWidget<T> {
                             ui.label("Description");
                         });
                         row.col(|ui| {
-                            let mut checked = metadata.contains_key(DESCRIPTION);
-                            if ui.checkbox(&mut checked, "").changed() {
-                                if checked {
-                                    metadata.insert(DESCRIPTION.to_owned(), String::new());
-                                } else {
-                                    metadata.remove(DESCRIPTION);
-                                }
-                            }
-                        });
-                        row.col(|ui| {
-                            if let Some(description) = metadata.get_mut(DESCRIPTION) {
-                                if ui
-                                    .add(
-                                        TextEdit::multiline(description)
-                                            .desired_width(f32::INFINITY),
-                                    )
-                                    .lost_focus()
-                                {
-                                    *description = description.trim().to_owned();
-                                }
+                            let description = metadata.entry(DESCRIPTION.to_owned()).or_default();
+                            if ui
+                                .add(TextEdit::multiline(description).desired_width(f32::INFINITY))
+                                .lost_focus()
+                            {
+                                *description = description.trim().to_owned();
                             }
                         });
                     });
@@ -191,44 +177,221 @@ impl<T: BorrowMut<Metadata>> MetadataWidget<T> {
                         row.col(|ui| {
                             ui.label("Authors");
                         });
-                        row.col(|ui| {});
                         row.col(|ui| {
-                            if let Some(authors) = metadata.get_mut(AUTHORS) {
-                                let mut authors: Vec<_> =
-                                    authors.split(",").map(str::trim).collect();
+                            let value = metadata.entry(AUTHORS.to_owned()).or_default();
+                            let mut authors = ui.memory_mut(|memory| {
+                                memory.caches.cache::<AuthorsComputed>().get(value)
+                            });
+                            let mut changed = false;
+                            authors.retain_mut(|author| {
+                                let mut keep = true;
+                                ui.horizontal(|ui| {
+                                    keep = !ui.button(MINUS).clicked();
+                                    changed |= !keep;
+                                    let response = ui.add(
+                                        TextEdit::singleline(author).desired_width(f32::INFINITY),
+                                    );
+                                    changed |= response.changed();
+                                    if response.lost_focus() {
+                                        *author = author.trim().to_owned();
+                                        changed = true;
+                                    }
+                                });
+                                keep
+                            });
+                            if changed {
+                                *value = authors.join(",");
+                            }
+                            if ui.button(PLUS).clicked() {
+                                value.push(',');
                             }
                         });
                     });
                 }
-                //             if let Some(authors) = metadata.get_mut(AUTHORS) {
-                //                 body.row(height, |mut row| {
-                //                     row.col(|ui| {
-                //                         ui.label("Authors");
-                //                     });
-                //                     let mut authors: Vec<_> = authors.split(",").map(str::trim).collect();
-                //                     row.col(|ui| {
-                //                         authors.retain_mut(|author| {
-                //                             let mut keep = true;
-                //                             ui.horizontal(|ui| {
-                //                                 keep = !ui.button(MINUS).clicked();
-                //                                 if ui
-                //                                     .add(TextEdit::singleline(author).desired_width(f32::INFINITY))
-                //                                     .lost_focus()
-                //                                 {
-                //                                     *author = author.trim().to_owned();
-                //                                 }
-                //                             });
-                //                             keep
-                //                         });
-                //                         if ui.button(PLUS).clicked() {
-                //                             authors.push(String::new());
-                //                         }
-                //                     });
-                //                 });
-                //             }
+                // Version
+                if self.version {
+                    body.row(height, |mut row| {
+                        row.col(|ui| {
+                            ui.label("Version");
+                        });
+                        row.col(|ui| {
+                            ui.horizontal(|ui| {
+                                let value = metadata.entry(VERSION.to_owned()).or_default();
+                                let mut version = Version::parse(value).unwrap_or_else(|error| {
+                                    error!(%error);
+                                    let version = Version::new(0, 0, 0);
+                                    *value = version.to_string();
+                                    version
+                                });
+                                let mut changed = false;
+                                ui.menu_button(version.to_string(), |ui| {
+                                    ui.visuals_mut().widgets.inactive = ui.visuals().widgets.active;
+                                    ui.horizontal(|ui| {
+                                        changed |=
+                                            ui.add(DragValue::new(&mut version.major)).changed();
+                                        changed |=
+                                            ui.add(DragValue::new(&mut version.minor)).changed();
+                                        changed |=
+                                            ui.add(DragValue::new(&mut version.patch)).changed();
+                                    });
+                                });
+                                if changed {
+                                    *value = version.to_string();
+                                }
+                            });
+                        });
+                    });
+                }
+                // Date
+                if self.date {
+                    body.row(height, |mut row| {
+                        row.col(|ui| {
+                            ui.label("Date");
+                        });
+                        row.col(|ui| {
+                            ui.horizontal(|ui| {
+                                let value = metadata.entry(DATE.to_owned()).or_default();
+                                let mut date = NaiveDate::parse_from_str(value, DATE_FORMAT)
+                                    .unwrap_or_else(|error| {
+                                        error!(%error);
+                                        let date = NaiveDate::default();
+                                        *value = date.to_string();
+                                        date
+                                    });
+                                if ui
+                                    .add(DatePickerButton::new(&mut date).show_icon(false))
+                                    .changed()
+                                {
+                                    *value = date.to_string();
+                                }
+                            });
+                        });
+                    });
+                }
             });
     }
 }
+
+/// Authors computed
+type AuthorsComputed = FrameCache<Vec<String>, AuthorsComputer>;
+
+/// Authors computer
+#[derive(Default)]
+struct AuthorsComputer;
+
+impl ComputerMut<&str, Vec<String>> for AuthorsComputer {
+    fn compute(&mut self, key: &str) -> Vec<String> {
+        key.split(',').map(ToOwned::to_owned).collect()
+    }
+}
+
+// /// Writable
+// fn writable(metadata: &mut Metadata, ui: &mut Ui) {
+//     ui.style_mut().visuals.collapsing_header_frame = true;
+//     let height = ui.spacing().interact_size.y;
+//     TableBuilder::new(ui)
+//         .resizable(true)
+//         .column(Column::auto())
+//         .column(Column::remainder())
+//         .body(|mut body| {
+//             // Name
+//             body.row(height, |mut row| {
+//                 row.col(|ui| {
+//                     ui.label("Name");
+//                 });
+//                 row.col(|ui| {
+//                     if ui
+//                         .add(TextEdit::singleline(&mut metadata.name).desired_width(f32::INFINITY))
+//                         .lost_focus()
+//                     {
+//                         metadata.name = metadata.name.trim().to_owned();
+//                     }
+//                 });
+//             });
+//             // Description
+//             body.row(height, |mut row| {
+//                 row.col(|ui| {
+//                     ui.label("Description");
+//                 });
+//                 row.col(|ui| {
+//                     if ui
+//                         .add(
+//                             TextEdit::multiline(&mut metadata.description)
+//                                 .desired_width(f32::INFINITY),
+//                         )
+//                         .lost_focus()
+//                     {
+//                         metadata.description = metadata.description.trim().to_owned();
+//                     }
+//                 });
+//             });
+//             // Authors
+//             body.row(height, |mut row| {
+//                 row.col(|ui| {
+//                     ui.label("Authors");
+//                 });
+//                 row.col(|ui| {
+//                     metadata.authors.retain_mut(|author| {
+//                         let mut keep = true;
+//                         ui.horizontal(|ui| {
+//                             keep = !ui.button(MINUS).clicked();
+//                             if ui
+//                                 .add(TextEdit::singleline(author).desired_width(f32::INFINITY))
+//                                 .lost_focus()
+//                             {
+//                                 *author = author.trim().to_owned();
+//                             }
+//                         });
+//                         keep
+//                     });
+//                     if ui.button(PLUS).clicked() {
+//                         metadata.authors.push(String::new());
+//                     }
+//                 });
+//             });
+//             // Version
+//             body.row(height, |mut row| {
+//                 row.col(|ui| {
+//                     ui.label("Version");
+//                 });
+//                 row.col(|ui| {
+//                     ui.horizontal(|ui| {
+//                         let mut checked = metadata.version.is_some();
+//                         if ui.checkbox(&mut checked, "").changed() {
+//                             metadata.version = checked.then_some(Version::new(0, 0, 0));
+//                         }
+//                         if let Some(version) = &mut metadata.version {
+//                             ui.menu_button(version.to_string(), |ui| {
+//                                 ui.visuals_mut().widgets.inactive = ui.visuals().widgets.active;
+//                                 ui.horizontal(|ui| {
+//                                     ui.add(DragValue::new(&mut version.major));
+//                                     ui.add(DragValue::new(&mut version.minor));
+//                                     ui.add(DragValue::new(&mut version.patch));
+//                                 });
+//                             });
+//                         }
+//                     });
+//                 });
+//             });
+//             // Date
+//             body.row(height, |mut row| {
+//                 row.col(|ui| {
+//                     ui.label("Date");
+//                 });
+//                 row.col(|ui| {
+//                     ui.horizontal(|ui| {
+//                         let mut checked = metadata.date.is_some();
+//                         if ui.checkbox(&mut checked, "").changed() {
+//                             metadata.date = checked.then_some(Local::now().date_naive());
+//                         }
+//                         if let Some(date) = &mut metadata.date {
+//                             ui.add(DatePickerButton::new(date).show_icon(false));
+//                         }
+//                     });
+//                 });
+//             });
+//         });
+// }
 
 // /// Writable
 // fn writable(metadata: &mut Metadata, ui: &mut Ui) {
